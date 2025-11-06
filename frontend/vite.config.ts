0b50fa3da7710f20eceeee5dff5b5f19f0d2c4f0
Gary Mazzeo <gmazzeo@zingermans.com>
2025-11-05 23:40:59 -0500

    Fix TypeScript Preprocessing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { resolve } from 'node:path';
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte({
+    preprocess: vitePreprocess(),
+  })],
   root: '.',
   build: {
     outDir: resolve(__dirname, '../public/assets'),
